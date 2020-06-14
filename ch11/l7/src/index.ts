c2bdc938dfdc30c01b2fae622d3f1a05a033b076
Carl Rippon <carl.rippon@googlemail.com>
2020-06-14 17:15:31 +0100

    l7 fixes

--- ch11/l7/src/index.ts
+++ ch11/l7/src/index.ts
@@ -2,8 +2,8 @@ function reducePrice(code: string, price: number, age: number) {
   const newPrice = price;
   switch (code) {
     case "TEN":
-      price = price - price * 0.1;
+      return price - price * 0.1;
     case "FIVE":
-      price = price - price * 0.1;
+      price = price - price * 0.05;
   }
 }
